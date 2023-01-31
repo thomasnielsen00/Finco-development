a206f16ada5c2879b6a625ec5e21933e1dc61b13
thomasnielsen00 <you@example.com>
2023-01-31 11:54:26 +0100

    remove gitignore db

---
+++ server/test/config.ts
@@ -0,0 +1,4 @@
+process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
+process.env.MYSQL_USER = 'username_todo';
+process.env.MYSQL_PASSWORD = 'username_todo';
+process.env.MYSQL_DATABASE = 'username_todo_test';
