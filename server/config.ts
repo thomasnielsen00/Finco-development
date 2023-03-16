924c761c858e64cf84eaa1e438ca742901490ade
thomasnielsen00 <you@example.com>
2023-03-16 22:02:47 +0100

    connected to google cloud sql :))))

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,9 @@
-process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
-process.env.MYSQL_USER = 'bknielse_fincoDB';
-process.env.MYSQL_PASSWORD = '123';
-process.env.MYSQL_DATABASE = 'bknielse_fincoDB_dev';
+process.env.MYSQL_HOST = '35.228.18.247';
+process.env.MYSQL_USER = 'ADMIN';
+process.env.MYSQL_PASSWORD = 'l@XuS-G~$a$Mp)"C';
+process.env.MYSQL_DATABASE = 'finco-dev';
+process.env.CLOUD_SQL_CONNECTION_NAME = 'proven-aviary-378108:europe-north1:finco-dev-db';
+// process.env.INSTANCE_UNIX_SOCKET = 'proven-aviary-378108:europe-north1:finco-dev-db';
+
+// passord: l@XuS-G~$a$Mp)"C
+// remeber to Authorize network
