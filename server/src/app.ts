ca6b713a276ebd3d98294f401316bd9f2a0de84f
thomasnielsen00 <you@example.com>
2023-03-22 12:22:52 +0100

    123

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import taskRouter from './finco-router';
 import userRouter from './user-router';
+import path from 'path';
 
 /**
  * Express application.
@@ -11,5 +12,13 @@ app.use(express.json());
 
 // Since API is not compatible with v1, API version is increased to v2
 app.use('/api/v2', taskRouter, userRouter);
+
+// Serve client files, un-comment this when developing
+// app.use(express.static(path.join(__dirname, '/../../client/public')));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.info(`Server running on port ${port}`);
+});
 
 export default app;
